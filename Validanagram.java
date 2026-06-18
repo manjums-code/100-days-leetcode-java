68515817da8858eb938009cf19f3e8cc2561bad7
manjums-code <manjushreemurugan.dev@gmail.com>
2026-06-18 23:32:25 +0530

    Add files via upload

---
+++ Validanagram.java
@@ -0,0 +1,19 @@
+public class Validanagram {
+    public boolean isAnagram(String s, String t) {
+         int []buc=new int[26];
+         for(int i=0;i<s.length();i++)
+         {
+            buc[s.charAt(i)-'a']++;
+         }
+         for(int i=0;i<t.length();i++)
+         {
+            buc[t.charAt(i)-'a']--;
+         }
+         for(int i=0;i<26;i++)
+         {
+            if(buc[i]!=0)
+            return false;
+         }
+         return true;
+    }
+}
