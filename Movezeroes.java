78eb156056dac723001816b2993e8ba5f25ab964
manjums-code <manjushreemurugan.dev@gmail.com>
2026-06-19 23:33:44 +0530

    Add files via upload

---
+++ Movezeroes.java
@@ -0,0 +1,15 @@
+public class Movezeroes {
+     public void moveZeroes(int[] nums) {
+        int temp=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {   
+                nums[temp]=nums[i];
+                if(temp!=i)
+                nums[i]=0;
+                temp++;
+            }
+        }
+    }
+}
