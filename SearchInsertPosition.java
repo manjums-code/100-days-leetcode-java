69ecaee172e9a8c61557e9d11fa77dc6c1baedb2
manjums-code <manjushreemurugan.dev@gmail.com>
2026-06-25 23:42:56 +0530

    Add files via upload

---
+++ SearchInsertPosition.java
@@ -0,0 +1,12 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int i=0;
+        for(i=0;i<nums.length;i++)
+        {
+          if(nums[i]==target||nums[i]>target)
+          return i;
+
+        }
+        return i;
+    }
+}
