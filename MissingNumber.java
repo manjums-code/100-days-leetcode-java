0f5f5fc29ca66ada8ae31a69a5709178ddbb7698
manjums-code <manjushreemurugan.dev@gmail.com>
2026-06-28 22:53:37 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+   public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[0]!=0)
+            return 0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+           if((nums[i]+1)!=nums[i+1])
+           return nums[i]+1;
+        }
+        return nums[nums.length-1]+1;
+    }
+}
