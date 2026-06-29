8e91bfd8ffc459b700e6d66d4afadee05128e985
manjums-code <manjushreemurugan.dev@gmail.com>
2026-06-30 00:20:57 +0530

    Add files via upload

---
+++ ValidParanthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')
+            {
+               stack.push(s.charAt(i));
+            }
+            else
+            {
+                if(!stack.isEmpty())
+                {
+                    if(s.charAt(i)==')' && stack.peek()=='(' || s.charAt(i)==']' && stack.peek()=='[' || s.charAt(i)=='}' && stack.peek()=='{')
+                    stack.pop();
+                    else
+                    return false;
+                }
+                 else
+                 return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
